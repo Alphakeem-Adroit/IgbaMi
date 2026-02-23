95b44fe629f6418943a44945b234138e5dc4f029
Alphakeem-Adroit <alphakeem12@gmail.com>
2026-02-24 00:45:11 +0100

    Chore: Serializing Models

--- igbami_backend/products/views.py
+++ igbami_backend/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductSerializer
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
